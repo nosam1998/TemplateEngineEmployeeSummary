a7c2c4c920a74c5da6884fa854dc08889f6e1d14
nosam1998 <nosam1998@gmail.com>
2020-09-22 16:58:09 -0700

    Cleaned up code

--- Develop/app.js
+++ Develop/app.js
@@ -12,30 +12,8 @@ const render = require("./lib/htmlRenderer");
 const Employee = require("./lib/Employee");
 
 
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
-
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
-
 let team_member_objs = [];
+
 
 const generic_info = [{
     type: "input",
@@ -51,11 +29,13 @@ const generic_info = [{
     name: "id"
 }];
 
+
 const get_manager_info = () => {
     inquirer.prompt(get_role_specific("Manager")).then(response => {
         create_employee_obj(response, "Manager");
     });
 }
+
 
 const get_employee_info = role => {
     inquirer.prompt(get_role_specific(role)).then(response => {
@@ -63,9 +43,10 @@ const get_employee_info = role => {
     });
 }
 
+
 const get_role_specific = role => {
     let gi = generic_info.slice(0);
-    console.log(generic_info);
+
     if (role === "Manager") {
         gi.push({
             type: "input",
@@ -91,6 +72,7 @@ const get_role_specific = role => {
     return gi;
 }
 
+
 const create_employee_obj = (response, role) => {
     console.log(role)
     if (role === "Manager") {
@@ -111,16 +93,13 @@ const create_employee_obj = (response, role) => {
 }
 
 
-// Might need to be a regular function (NOT an arrow function)
 const get_role = () => {
-    // Get the information specific to the type/role of the employee
     inquirer.prompt([{
         type: "list",
         name: "role",
         message: "What is the employees role?",
         choices: ["Engineer", "Intern", "No more employees to add."]
     }]).then(response => {
-        // console.log(response);
         if (response.role === "No more employees to add.") {
             render_html();
         } else {
@@ -135,12 +114,9 @@ const render_html = () => {
         console.log("Please add at least one employee before you are done!")
     } else {
         let html = render(team_member_objs)
-        // console.log(html);
         fs.writeFileSync("templates/index.html", html);
     }
 }
 
-get_manager_info();
 
-// let role = get_role();
-// get_generic_info(role);
+get_manager_info();
